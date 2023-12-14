ca38ff7cfb72df2c7e9fc3712fab86a8082452ee
CodeRuner5235 <72626572+CodeRuner5235@users.noreply.github.com>
2023-12-13 19:07:27 -0500

    Add files via upload

---
+++ ppt18w/ppt18w.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#check current power limit
+slowppt_limit=$(cat /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon5/power1_cap)
+fastppt_limit=$(cat /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon5/power2_cap)
+
+#adjust based on value
+if [ "$slowppt_limit" -eq 15000000 ]; then 
+	echo 18000000 > /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon5/power1_cap
+fi
+
+if [ "$fastppt_limit" -eq 15000000 ]; then
+	echo 18000000 > /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon5/power2_cap
+fi
