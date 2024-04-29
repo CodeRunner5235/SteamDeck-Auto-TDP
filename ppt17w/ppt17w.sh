9e240e796d8a6929795ef5183212109aabe194dd
Brandon Willett <pickledish@users.noreply.github.com>
2024-04-28 20:34:22 -0400

    edit path for hwmon

--- ppt17w/ppt17w.sh
+++ ppt17w/ppt17w.sh
@@ -2,14 +2,14 @@
 
 
 #check current power limit
-slowppt_limit=$(cat /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon5/power1_cap)
-fastppt_limit=$(cat /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon5/power2_cap)
+slowppt_limit=$(cat /sys/class/hwmon/hwmon6/power1_cap)
+fastppt_limit=$(cat /sys/class/hwmon/hwmon6/power2_cap)
 
 #adjust based on value
 if [ "$slowppt_limit" -eq 15000000 ]; then 
-	echo 17000000 > /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon5/power1_cap
+	echo 17000000 > /sys/class/hwmon/hwmon6/power1_cap
 fi
 
 if [ "$fastppt_limit" -eq 15000000 ]; then
-	echo 17000000 > /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon5/power2_cap
+	echo 17000000 > /sys/class/hwmon/hwmon6/power2_cap
 fi
